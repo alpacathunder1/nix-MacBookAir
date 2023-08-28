2687819abfccac96717f0d5ef40158dea9f19bdd
Alex Taylor <ataylor@posteo.us>
2023-08-28 14:13:48 -0400

    added common nvim aliases

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -32,6 +32,9 @@
   # Create /etc/zshrc that loads the nix-darwin environment.
   programs.zsh.enable = true;  # default shell on catalina
   # programs.fish.enable = true;
+  environment.shellAliases.vi = "nvim";
+  environment.shellAliases.vim = "nvim";
+  environment.shellAliases.view = "nvim -R \"$@\"";
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
