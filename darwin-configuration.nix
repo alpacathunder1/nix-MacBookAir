da40f2d9912c80a5334c6619515691698158dc67
Alex Taylor <ataylor@posteo.us>
2023-09-13 18:11:12 -0400

    enable darkmode

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -24,6 +24,7 @@
       EDITOR = "nvim";
   };
 
+  system.defaults.NSGlobalDomain.AppleInterfaceStyle = "Dark";
   # Use a custom configuration.nix location.
   # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
   # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
