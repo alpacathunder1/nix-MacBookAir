78c8e7fc375f9ccfccda645c7f823b0fb12e2065
Alex Taylor <ataylor@posteo.us>
2023-10-14 12:41:20 -0400

    added brew update to nixUp alias

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -76,7 +76,8 @@
     PROMPT="%n %~ $ " && bindkey '^R' history-incremental-search-backward && test -f /tmp/unlock || ssh-add --apple-use-keychain ~/.ssh/id_rsa && touch /tmp/unlock'';
   # programs.fish.enable = true;
   ## Nix Aliases
-  environment.shellAliases.nixUp = "nix-channel --update;darwin-rebuild switch";
+  environment.shellAliases.nixUp =
+    "nix-channel --update;darwin-rebuild switch;brew update;brew upgrade";
   ## neovim aliases
   environment.shellAliases.vi = "nvim";
   environment.shellAliases.vim = "nvim";
