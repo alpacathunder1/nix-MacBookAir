9344fe8e3cdab560d3f2c9d0b2fdd477fea0e907
Alex Taylor <ataylor@posteo.us>
2023-12-27 11:07:24 -0500

    quoting to prevent shellcheck error

--- postrun.sh
+++ postrun.sh
@@ -2,4 +2,4 @@
 # I couldn't get the shell to be modified within nix for now, so I'm just "documenting" this as a dumb script
 # https://gist.github.com/idleberg/9c7aaa3abedc58694df5
 sudo bash -c 'echo $(which fish) >> /etc/shells'
-chsh -s $(which fish)
+chsh -s "$(which fish)"
